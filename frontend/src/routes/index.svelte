81ad50007ace2c1ab739bf883da29f265628aee1
Anmol Ratn <anmol.ratn@outlook.com>
2022-06-27 15:06:09 +0530

    ui changes

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -2,10 +2,27 @@
     import { Button, Icon } from "sveltestrap";
     import FormData from "form-data";
 
-    let fileinput;
+    let fileInput;
+    let cameraInput;
     let src;
     let text;
     let displayText;
+
+    let mobileCheck = function () {
+        let check = false;
+        (function (a) {
+            if (
+                /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
+                    a
+                ) ||
+                /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+                    a.substr(0, 4)
+                )
+            )
+                check = true;
+        })(navigator.userAgent || navigator.vendor || window.opera);
+        return check;
+    };
 
     const onFileSelected = (e) => {
         src = null;
@@ -38,32 +55,55 @@
 </script>
 
 <div id="app">
-    <h1 class="text">Label Your Image</h1>
-    {#if src}
-        <img {src} alt="placeholder" id="image-space" />
-    {:else}
-        <img src="placeholder.png" alt="placeholder" id="image-space" />
-    {/if}
-    {#if displayText}<p>Loading...</p>{/if}
+    <h1 class="text app-title">LABEL YOUR IMAGE</h1>
+    <div id="image-parent">
+        <img src="border.jpg" alt="border" id="image-border" />
+        {#if src}
+            <img {src} alt="placeholder" id="image-space" />
+        {:else}
+            <img src="placeholder.png" alt="placeholder" id="image-space" />
+        {/if}
+    </div>
+
+    {#if displayText}<p class="text" id="loading-text">Loading...</p>{/if}
     {#if text}
-        <ol>
-            <li>{text[0]}</li>
-            <li>{text[1]}</li>
-            <li>{text[2]}</li>
+        <ol id="label-list">
+            <li class="text">{text[0]}</li>
+            <li class="text">{text[1]}</li>
+            <li class="text">{text[2]}</li>
         </ol>
     {/if}
-    <Button
-        on:click={() => {
-            fileinput.click();
-        }}>Upload/Capture Image <Icon name="upload" /></Button
-    >
+    <div>
+        {#if mobileCheck}
+            <Button
+                class="action-button"
+                on:click={() => {
+                    cameraInput.click();
+                }}>Capture Image <Icon name="camera" /></Button
+            >
+        {/if}
+        <Button
+            class="action-button"
+            on:click={() => {
+                fileInput.click();
+            }}>Upload Image <Icon name="upload" /></Button
+        >
+    </div>
+
     <input
         style="display:none"
         type="file"
         accept="image/*"
         capture
         on:change={(e) => onFileSelected(e)}
-        bind:this={fileinput}
+        bind:this={cameraInput}
+    />
+    <input
+        style="display:none"
+        type="file"
+        accept="image/*"
+        on:change={(e) => onFileSelected(e)}
+        bind:this={fileInput}
     />
 </div>
 
@@ -73,17 +113,53 @@
         align-items: center;
         justify-content: center;
         flex-flow: column;
-        margin-top: 3em;
+        margin-top: 2em;
     }
 
     .text {
-        font-family: Arial, Helvetica, sans-serif;
+        font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+            "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+        color: azure;
+    }
+
+    .app-title {
+        font-family: "Luckiest Guy", cursive;
+        font-size: 2.5rem;
+        margin: 0.5em;
+    }
+
+    #image-parent {
+        position: relative;
+        top: 0;
+        left: 0;
+    }
+
+    #image-border {
+        position: relative;
+        top: 0;
+        left: 0;
+        height: 450px;
+        width: 300px;
+        z-index: 0;
     }
 
     #image-space {
-        margin: 1em;
-        height: 224px;
-        width: 224px;
-        outline: auto;
+        position: absolute;
+        top: 175px;
+        left: 25px;
+        height: 250px;
+        width: 250px;
+        z-index: 2;
+    }
+
+    #label-list,
+    #loading-text {
+        margin: 2em;
+    }
+
+    :global(.action-button) {
+        margin-top: 2em;
+        margin-left: 0.5rem;
+        margin-right: 0.5rem;
     }
 </style>
